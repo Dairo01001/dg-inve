a9ce2600d07dba8d1b158a812e6d08931dc6bc3e
Dairo Garcia Naranjo <garcianaranjodairo@gmail.com>
2026-03-20 17:31:49 -0500

    Add API response decorators for user-related endpoints

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -12,17 +12,20 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserEntity } from './entities/user.entity';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: UserEntity })
   async signupUser(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
     return this.usersService.createUser(createUserDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: [UserEntity] })
   async getUsers(): Promise<UserEntity[]> {
     return this.usersService.users({
       orderBy: { createdAt: 'desc' },
@@ -30,6 +33,7 @@ export class UsersController {
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: UserEntity })
   async getUserById(@Param('id') id: number): Promise<UserEntity> {
     const user = await this.usersService.user({ id: id });
     if (!user) {
@@ -39,7 +43,8 @@ export class UsersController {
   }
 
   @Patch(':id')
-  async update(
+  @ApiOkResponse({ type: UserEntity })
+  async updateUserById(
     @Param('id') id: number,
     @Body() updateUserDto: UpdateUserDto,
   ): Promise<UserEntity> {
@@ -50,7 +55,8 @@ export class UsersController {
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: number): Promise<UserEntity> {
+  @ApiOkResponse({ type: UserEntity })
+  async deleteUserById(@Param('id') id: number): Promise<UserEntity> {
     return this.usersService.deleteUser({ id });
   }
 }
