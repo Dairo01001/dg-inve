da6bb6b66ce9eecd9830b1dbffcbb97982ed9d89
Dairo Garcia Naranjo <garcianaranjodairo@gmail.com>
2026-03-22 11:31:45 -0500

    Add Role model and update User model with roleId and deletedAt fields

---
+++ prisma/migrations/20260322155638_add_role_model/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - Added the required column `roleId` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "deletedAt" TIMESTAMP(3),
+ADD COLUMN     "roleId" INTEGER NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Role_name_key" ON "Role"("name");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
